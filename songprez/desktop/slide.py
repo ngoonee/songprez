62fd48749a30817d55a1b537b44a4bc5215d3fa9
Ng Oon-Ee <ngoonee@gmail.com>
2015-11-28 06:19:21 +0800

    Optimizations to speed up slide creation, as its already creating a noticeable lag even with half a dozen songs and my i7...

--- songprez/desktop/slide.py
+++ songprez/desktop/slide.py
@@ -43,7 +43,7 @@ class SlideElement(BoxLayout):
                                       'thin', 'minimal', 'none'])
 
     def on_padding(self, instance, val):
-        print(val)
+        pass
 
 
 class OutlinedLabel(Label, StencilView):
@@ -51,6 +51,16 @@ class OutlinedLabel(Label, StencilView):
     target_font_size = NumericProperty(0)
     border_thickness = OptionProperty('regular', options=['thick', 'regular',
                                       'thin', 'minimal', 'none'])
+    '''
+    Cached value of last best font size found, used to initialize the next
+    search. Worst case performance identical to search from scratch, best case
+    is less than half the iterations. Cuts down search time by about 1/3rd.
+
+    This is accessed using the class, not the instance, i.e.
+        OutlinedLabel._candidate_font_size
+        not myLabel._candidate_font_size or self._candidate_font_size
+    '''
+    _candidate_font_size = 0
 
     def __init__(self, **kwargs):
         super(OutlinedLabel, self).__init__(**kwargs)
@@ -75,30 +85,58 @@ class OutlinedLabel(Label, StencilView):
             del self._outline[:]
         except AttributeError:
             pass
+        if self.size[1] <= 100:
+            '''
+            Default size of 100 or 0 is unlikely to ever appear in the wild,
+            so ignore that case to cut out half of the possible iterations.
+            '''
+            return
         with self.canvas.before:
             self.font_size = self.target_font_size
             self.texture_update()
             if self.texture_size[1] > self.size[1]:
                 '''
-                Binary search (only if font size is too big, if too small
-                nevermind. Find a font size in between current and smallest
-                and test again. If its small enough, find a font size between
-                current and biggest.
+                Simple interpolation search to be run if font size is too big.
+                Attempts to find the largest font size which would fit in
+                self.size. Candidate is estimated from the ratio of current
+                height to desired height, with a fall back to binary search
+                (mid point) if the estimate is obviously wrong.
                 '''
                 upper = self.target_font_size
                 lower = self.min_font_size
-                while upper-lower > 1:
+                # Bootstrap using a class variable cache (if it is not 0)
+                if OutlinedLabel._candidate_font_size:
+                    candidate = OutlinedLabel._candidate_font_size
+                else:  # Otherwise just initialize with mid-point
                     candidate = (upper + lower) // 2
+                # Stop when there are no more integers to search
+                while upper-lower > 1:
+                    # Apply the candidate font size and test it, resetting
+                    # upper and lower bounds
                     self.font_size = candidate
                     self.texture_update()
                     if self.texture_size[1] > self.size[1]:
                         upper = candidate
                     else:
                         lower = candidate
+                    # Obtain newer candidate font size using interpolation
+                    candidate = round(candidate * self.size[1]/self.texture_size[1])
+                    if candidate >= upper or candidate <= lower:
+                        # Fall back to binary if result would cause a loop
+                        candidate = (upper + lower) // 2
+                # At this point, upper is slightly too large (otherwise it
+                # would not have been assigned as upper limit) so pick lower
                 self.font_size = lower
-                self.texture_update()
-                # At this point, maximum allowable font size has been achieved
+                # Cache the selected font size value. TODO: multiple cached?
+                OutlinedLabel._candidate_font_size = lower
+                self.texture_update()  # Update texture to prepare for borders
 
+            '''
+            Draw borders around fonts, based on both font_size and a
+            border_thickness input. Basically duplicates current texture
+            which contains the text in a circle with offset according to
+            border thickness.
+            '''
             if self.border_thickness == 'none':
                 return
             scale = {'thick': 1.5,
