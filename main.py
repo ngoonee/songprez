80a22e6f59674614dde5836bc9b1f8a7ec2fd38c
Ng Oon-Ee <ngoonee@gmail.com>
2016-03-22 17:42:12 +0800

    Workaround in main.py so that all module calls to logging go to the Kivy log

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 __version__ = '0.1'
 import sys
+# Workaround so logging module calls go to the Kivy log
+from kivy.logger import Logger
+import logging
+logging.Logger.manager.root = Logger
 
 if __name__ == '__main__':
     if 'desktop' in sys.argv:
