19286098a7b443f32b3967f6fc6e256c7c415f97
Ng Oon-Ee <ngoonee@gmail.com>
2016-03-25 05:58:23 +0800

    Update settings pathex to use own custom layout rather than filebrowser from garden (which does not work well on mobile)

--- songprez/phone/settings.py
+++ songprez/phone/settings.py
@@ -3,20 +3,85 @@ import kivy
 # kivy.require('1.9.1')
 from kivy.lang import Builder
 from kivy.core.window import Window
-from kivy.metrics import dp
-from kivy.uix.settings import Settings, SettingItem
+from kivy.uix.settings import SettingsWithSpinner, SettingItem
 from kivy.uix.popup import Popup
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty, BooleanProperty
-from kivy.garden.filebrowser import FileBrowser
+from kivy.properties import ObjectProperty, BooleanProperty, StringProperty
+
 import os
 
 Builder.load_string("""
+#:import os os
 <SettingPathEx>:
     Label:
         text: root.value or ''
         pos: root.pos
+        text_size: root.width/3, None
+        shorten: True
+
+<PathPopup>:
+    accept: accept
+    cancel: cancel
+    filepath: filepath
+    browser: browser
+    title_font: self.font_name
+    title_size: '32sp'
+    BoxLayout:
+        orientation: 'vertical'
+        BoxLayout:
+            orientation: 'horizontal'
+            size_hint_y: None
+            height: '32sp'
+            Button:
+                text: 'Icon'
+                on_press: browser.view_mode = 'icon'
+            Button:
+                text: 'List'
+                on_press: browser.view_mode = 'list'
+        Label:
+            id: filepath
+            size_hint_y: None
+            height: '32sp'
+            halign: 'left'
+            text_size: self.width, None
+            shorten: True
+        FileChooser:
+            id: browser
+            path: root.path
+            dirselect: root.dirselect
+            FileChooserIconLayout
+            FileChooserListLayout
+        BoxLayout:
+            orientation: 'horizontal'
+            size_hint_y: None
+            height: '32sp'
+            Button:
+                id: accept
+                text: 'OK'
+            Button:
+                id: cancel
+                text: 'Cancel'
 """)
+
+
+class PathPopup(Popup):
+    dirselect = BooleanProperty(False)
+    path = StringProperty('')
+
+    def __init__(self, **kwargs):
+        super(PathPopup, self).__init__(**kwargs)
+        self.auto_dismiss = False
+
+    def _on_keyboard_down(self, *args):
+        keycode = args[1]
+        if keycode == 27:  # 'esc' on desktop, 'back' key on android
+            self.dismiss()
+            return True
+
+    def on_open(self):
+        Window.bind(on_key_down=self._on_keyboard_down)
+
+    def on_dismiss(self):
+        Window.unbind(on_key_down=self._on_keyboard_down)
 
 
 class SettingPathEx(SettingItem):
@@ -35,26 +100,30 @@ class SettingPathEx(SettingItem):
         self.popup = None
 
     def _validate(self, instance):
-        self._dismiss()
-        if len(instance.selection):
-            value = instance.selection[0]
-            if not value:
-                return
+        value = self.popup.filepath.text
+        if value:
             self.value = os.path.realpath(value)
+        self._dismiss()
 
     def _create_popup(self, instance):
-        content = BoxLayout()
-        self.popup = Popup(title=self.title, content=content,
-                           size_hint=(0.9, 0.9))
-        browser = FileBrowser(select_string='Select', path=self.value,
-                              dirselect=self.dirselect,
-                              show_hidden=self.show_hidden)
-        content.add_widget(browser)
-        browser.bind(on_success=self._validate, on_canceled=self._dismiss)
+        self.popup = PathPopup(title=self.title, size_hint=(0.9, 0.9),
+                               path=self.value, dirselect=self.dirselect)
+        dir_filter = lambda dir, elem: os.path.isdir(elem)
+        self.popup.browser.filters=[dir_filter]
+        def update_filepath(instance, value):
+            if len(value) == 0 or value[-1] == '':
+                self.popup.filepath.text = instance.path
+            elif value[-1] == '../':
+                self.popup.filepath.text = os.path.dirname(instance.path)
+            else:
+                self.popup.filepath.text = value[-1]
+        self.popup.browser.bind(selection=update_filepath)
+        self.popup.accept.bind(on_release=self._validate)
+        self.popup.cancel.bind(on_release=self._dismiss)
         self.popup.open()
 
 
-class SPSettings(Settings):
+class SPSettings(SettingsWithSpinner):
     def __init__(self, *args, **kwargs):
         super(SPSettings, self).__init__(*args, **kwargs)
         self.register_type('pathex', SettingPathEx)
