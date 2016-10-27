a60018a998cceeac030fc91363b24267aed0f6b2
Ng Oon-Ee <ngoonee@gmail.com>
2016-10-28 06:43:04 +0800

    Update material icons font to latest available version

--- songprez/phone/fontutil.py
+++ songprez/phone/fontutil.py
@@ -26,46 +26,47 @@ def font_register():
     LabelBase.register(name="NotoSans", fn_regular=notosans)
     LabelBase.register(name="NotoSansMono", fn_regular=notosansmono)
 
-icon_map = {'donate': u'\uf5b9',  # square-inc-cash
-            'menu': u'\uf44e',  # menu
-            'sets': u'\uf423',  # library
-            'present': u'\uf518',  # presentation-play
-            'songs': u'\uf30e',  # file-document
-            'scripture': u'\uf15e',  # audiobook (WILL BE BIBLE, KEEP CHECKING WEBSITE FOR UPDATE)
-            'settings': u'\uf582',  # settings
-            'search': u'\uf43b',  # magnify
-            'edit': u'\uf4da',  # pencil
-            'save': u'\uf289',  # content-save
-            'chordoff': u'\uf47b',  # music-note-off
-            'delete': u'\uf2b5',  # delete
-            'cancel': u'\uf250',  # close-circle
-            'ok': u'\f229',  # checkbox-marked-circle
-            'new': u'\uf504',  # plus
-            'listadd': u'\uf501',  # playlist-plus
-            'listremove': u'\uf502',  # playlist-remove
-            'sort': u'\uf5a8',  # sort
-            'copy': u'\uf285',  # content-copy
-            'saveas': u'\uf15c',  # at
-            'language': u'\uf5a9',  # alphabetical
-            'showset': u'\uf36b',  # format-indent-increase
-            'listmoveup': u'\uf157',  # arrow-up-bold
-            'listmovedown': u'\uf145',  # arrow-down-bold
-            'bibleversion': u'\uf5d3',  # sword
-            'plus': u'\uf508',  # plus-circle-outline
-            'transpose': u'\uf478',  # music-note
-            'expand': u'\uf238',  # chevron-right
-            'collapse': u'\uf236',  # chevron-down
-            '0': u'\uf491',  # numeric-0-box
-            '1': u'\uf494',  # numeric-1-box
-            '2': u'\uf497',  # numeric-2-box
-            '3': u'\uf49a',  # numeric-3-box
-            '4': u'\uf49d',  # numeric-4-box
-            '5': u'\uf4a0',  # numeric-5-box
-            '6': u'\uf4a3',  # numeric-6-box
-            '7': u'\uf4a6',  # numeric-7-box
-            '8': u'\uf4a9',  # numeric-8-box
-            '9': u'\uf4ac',  # numeric-9-box
-            '9+': u'\uf4af',  # numeric-9-plus-box
+icon_map = {'donate': u'\uf4cb',  # square-inc-cash
+            'menu': u'\uf35c',  # menu
+            'sets': u'\uf5da',  # book-open-page-variant
+            'present': u'\uf429',  # presentation-play
+            'songs': u'\uf219',  # file-document
+            'scripture': u'\uf0a2',  # bible
+            'settings': u'\uf493',  # settings
+            'search': u'\uf349',  # magnify
+            'edit': u'\uf3eb',  # pencil
+            'save': u'\uf193',  # content-save
+            'chordoff': u'\uf38a',  # music-note-off
+            'delete': u'\uf1c0',  # delete
+            'cancel': u'\uf159',  # close-circle
+            'ok': u'\f134',  # checkbox-marked-circle-outline
+            'new': u'\uf415',  # plus
+            'listadd': u'\uf412',  # playlist-plus
+            'listremove': u'\uf413',  # playlist-remove
+            'sort': u'\uf4ba',  # sort
+            'copy': u'\uf18f',  # content-copy
+            'saveas': u'\uf065',  # at
+            'language': u'\uf4bb',  # alphabetical
+            'showset': u'\uf276',  # format-indent-increase
+            'listmoveup': u'\uf05e',  # arrow-up-bold
+            'listmovedown': u'\uf046',  # arrow-down-bold
+            'bibleversion': u'\uf4e5',  # sword
+            'plus': u'\uf419',  # plus-circle-outline
+            'transpose': u'\uf387',  # music-note
+            'expand': u'\uf140',  # chevron-down
+            'collapse': u'\uf143',  # chevron-up
+            'reorder': u'\uf687',  # reorder-horizontal
+            '0': u'\uf3a1',  # numeric-0-box
+            '1': u'\uf3a4',  # numeric-1-box
+            '2': u'\uf3a7',  # numeric-2-box
+            '3': u'\uf3aa',  # numeric-3-box
+            '4': u'\uf3ad',  # numeric-4-box
+            '5': u'\uf3b0',  # numeric-5-box
+            '6': u'\uf3b3',  # numeric-6-box
+            '7': u'\uf3b6',  # numeric-7-box
+            '8': u'\uf3b9',  # numeric-8-box
+            '9': u'\uf3bc',  # numeric-9-box
+            '9+': u'\uf3bf',  # numeric-9-plus-box
             }
 
 def iconfont(name, size=None):
